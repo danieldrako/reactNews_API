a452dd33317242089967fb81264b3f61664c46f0
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-04-17 12:31:56 -0600

    aftert how can i use the components in MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Container, Grid, Typography } from "@mui/material"
 
 function App() {
   
 
   return (
-    <h1>APP noticias</h1>
+    <Container>
+      <header>
+        <Typography align='center' marginY={5} component="h1" variant="h3">
+          Buscador de Noticias
+        </Typography>
+      </header>
+    </Container>
   )
 }
 
