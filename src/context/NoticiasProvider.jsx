5be072139f1326618a3d6e57a299c16bc19d21f8
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-04-18 12:12:55 -0600

    after query the news about selected category

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,9 +1,22 @@
+import axios from "axios";
 import { useState, useEffect, createContext } from "react";
 
 const NoticiasContext = createContext()
 
 const NoticiasProvider = ({children})=> {
     const [categoria, setCategoria] = useState('general')
+    const [noticias, setNoticias] = useState([])
+ 
+    useEffect(() => { 
+        const consultarApi = async ()  => { 
+            const url = ` https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+            const {data} = await axios(url)
+            
+            setNoticias(data.article)
+         }
+         consultarApi()
+     },[categoria])
 
     const handleChangeCategoria = e =>{
         setCategoria(e.target.value)
