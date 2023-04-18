84c372120d53537dbec476b7933dd61dbeca656a
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-04-18 10:19:40 -0600

    after detect selected category

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -3,10 +3,18 @@ import { useState, useEffect, createContext } from "react";
 const NoticiasContext = createContext()
 
 const NoticiasProvider = ({children})=> {
+    const [categoria, setCategoria] = useState('general')
+
+    const handleChangeCategoria = e =>{
+        setCategoria(e.target.value)
+    }
 
     return(
         <NoticiasContext.Provider
-            value={{}}
+            value={{
+                categoria,
+                handleChangeCategoria
+            }}
         >
             {children}
         </NoticiasContext.Provider>
